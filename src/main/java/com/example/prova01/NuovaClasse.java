b4203a490a7accb09300078928629b1b329e2465
luca.mucciarone <luca.mucciarone@gmail.com>
2023-10-19 00:02:51 +0200

    prima modifica 01

--- src/main/java/com/example/prova01/NuovaClasse.java
+++ src/main/java/com/example/prova01/NuovaClasse.java
@@ -1,2 +1,15 @@
-package com.example.prova01;public class NuovaClasse {
+package com.example.prova01;
+
+public class NuovaClasse {
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int a;
+
+
 }
